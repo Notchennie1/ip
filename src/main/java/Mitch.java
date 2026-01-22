4967f6b7407e588125cb29f2ecaa14119fc7e54d
Notchennie1 <chenxian0603@gmail.com>
2026-01-23 07:11:16 +0800

    Level-0: rename, greet, exit

---
+++ src/main/java/Mitch.java
@@ -0,0 +1,11 @@
+public class Mitch {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello, I'm Mitch\nWhat can I do for you");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println(LINE);
+    }
+}
