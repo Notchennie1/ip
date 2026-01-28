63503739781730e297f2ed2eeb24f52ec9a1aaaa
Notchennie1 <chenxian0603@gmail.com>
2026-01-28 17:43:41 +0800

    Level-1

--- src/main/java/Mitch.java
+++ src/main/java/Mitch.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Mitch {
     private static final String LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
-        System.out.println(LINE);
-        System.out.println("Hello, I'm Mitch\nWhat can I do for you");
-        System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again!");
-        System.out.println(LINE);
+        System.out.println(LINE+"\n" + "Hello, I'm Mitch\nWhat can I do for you\n" + LINE);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            if (line.equals("bye")){
+                // Reply to bye and stop the loop
+                System.out.println(LINE + "\nBye. Hope to see you again!\n"+LINE);
+                break;
+            }
+            else{
+                // echo
+                System.out.println(LINE +"\n"+ line +"\n" + LINE);
+            }
+        }
+        sc.close();
+
     }
 }
